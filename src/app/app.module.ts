0db4d6d9bdba8e14eeeb6dd859ff6ce41ee1e14a
lauradillonar <laura.dillon.ar@gmail.com>
2022-07-04 12:25:38 -0300

    Creando la clase Producto, los componentes para las listas y el servicio producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
+import { ListaProductoComponent } from './producto/lista-producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto/editar-producto.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(), // ToastrModule added
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
